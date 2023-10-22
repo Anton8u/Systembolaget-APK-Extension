7f7854834c6136731bd623e9bd1c103ebde2bfae
Anton8u <antonrah@chalmers.se>
2023-10-22 19:54:40 +0200

    basic template

--- popup.js
+++ popup.js
@@ -1,12 +1,11 @@
 // popup.js
 
 document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("scrapeButton");
-  const messageDisplay = document.getElementById("messageDisplay");
+  const button = document.getElementById("addApk");
 
   button.addEventListener("click", () => {
     // Send a message to the background script when the button is pressed
-    chrome.runtime.sendMessage({ action: "scrapeData" });
+    chrome.runtime.sendMessage({ action: "addApk" });
   });
 
 });
