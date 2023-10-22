c1d0cf982137e041fe546131ce07e2fa246669d3
Anton8u <antonrah@chalmers.se>
2023-10-23 00:02:50 +0200

    working basically

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "reorderDivs") {
-    // Handle the message here, or you can even ignore it
-  }
-});
