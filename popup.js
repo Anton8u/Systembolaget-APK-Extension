f73f3a4cc4f68e5d9c657e4873239be87361ddab
Anton8u <antonrah@chalmers.se>
2023-10-23 14:00:47 +0200

    added langauge support thing, dont know if it work

--- popup.js
+++ popup.js
@@ -1,3 +1,29 @@
+// Detect the browser's preferred language
+const userLanguage = navigator.language;
+
+// Define the text content in both Swedish and English
+const languageText = {
+  SWE: {
+    title: "APK till Systembolaget",
+    button: "Sortera!",
+    credit: "av",
+  },
+  ENG: {
+    title: "APK for Systembolaget",
+    button: "Sort!",
+    credit: "by",
+  },
+};
+
+const text = userLanguage.startsWith("sv") ? languageText.SWE : languageText.ENG;
+
+// Update the popup's content
+document.querySelector('.title').textContent = text.title;
+document.querySelector('#reorderButton').textContent = text.button;
+document.querySelector('.credit').innerHTML = text.credit + ' <a href="https://github.com/anton8u" style="color: #FCD205;" target="_blank">Anton8u</a>';
+
+
+
 function func(reorder) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const tab = tabs[0];
