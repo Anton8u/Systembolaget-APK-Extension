7638eb0f42c77ec5691852498081c995eea3e418
Anton8u <antonrah@student.chalmers.se>
2024-12-15 11:02:20 +0100

    branch finished. productPage works. search still has issues

---
+++ jsonToDict.py
@@ -0,0 +1,15 @@
+import json
+
+with open('products-2024-12-15.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+with open('data.txt', 'w') as output_file:
+    for item in data:
+        productNumber = item.get('productNumber')
+        vol = item.get('volume')
+        alc = item.get('alcoholPercentage')
+        price = item.get('price')
+        apk = round(vol * alc / price)
+
+        
+        output_file.write(f"'{productNumber}': {apk},\n")
